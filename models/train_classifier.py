a45a6a8207bb7d024e0bcda3ac4a584834df426c
Heegyu Kim <heekue83@gmail.com>
2020-10-03 23:13:49 +0900

    initial commit(forced)

---
+++ models/train_classifier.py
@@ -0,0 +1,102 @@
+import sys
+import string
+import pickle
+
+import pandas as pd
+
+from sqlalchemy import create_engine
+
+import nltk
+from nltk.corpus import stopwords  
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+
+
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table("Message", engine)
+    X = df.message
+    Y = df.iloc[:, 5:]
+    return X, Y, list(Y.columns)
+
+
+skip_tokens = stopwords.words('english') + ["'s", "n't"] + list(string.punctuation)
+
+def tokenize(text):
+    lemm = WordNetLemmatizer()
+    
+    text = word_tokenize(text)
+    text = [lemm.lemmatize(x).lower().strip() for x in text]
+    text = filter(lambda x: x not in skip_tokens, text)
+    
+    return " ".join(text)
+    
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    reports = []
+    y_pred = model.predict(X_test)
+    y_pred = pd.DataFrame(y_pred)
+    y_pred.columns = Y_test.columns
+    
+    for column in Y_test.columns:
+        report = classification_report(Y_test[column], y_pred[column])
+        reports.append(report)
+
+        print("Classification report for {}".format(column))
+        print(report)
+
+
+def save_model(model, model_filepath):
+    with open(model_filepath, "wb") as f:
+        pickle.dump(model, f)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
